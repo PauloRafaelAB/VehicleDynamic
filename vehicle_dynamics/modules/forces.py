897acfdce607f33ffaa200c777c1913ea21c7ca1
Yuri <42354569+poledna@users.noreply.github.com>
2023-07-17 18:15:53 +0200

    add y forces

--- vehicle_dynamics/modules/forces.py
+++ vehicle_dynamics/modules/forces.py
@@ -5,19 +5,30 @@ path_to_simulation_data = "../../exampledata/lanechange/SimulationData.pickle"
 sim_data = import_data_CM(path_to_simulation_data)
 
 sum_wheel_forces = []
+y_forces = []
 for i in range(15003):
     wheel_forces = np.array([[sim_data[i].wheel_load_x_FL, sim_data[i].wheel_load_x_RL, sim_data[i].wheel_load_x_FR, sim_data[i].wheel_load_x_RR],
                              [sim_data[i].wheel_load_y_FL, sim_data[i].wheel_load_y_RL, sim_data[i].wheel_load_y_FR, sim_data[i].wheel_load_y_RR],
                              [sim_data[i].wheel_load_z_FL, sim_data[i].wheel_load_z_RL, sim_data[i].wheel_load_z_FR, sim_data[i].wheel_load_z_RR]])
-
+    y_forces .append([sim_data[i].wheel_load_y_FL, sim_data[i].wheel_load_y_RL, sim_data[i].wheel_load_y_FR, sim_data[i].wheel_load_y_RR])
     sum_wheel_forces.append(np.sum(wheel_forces, axis=1).tolist())
 
 
 plt.title("forces")
 sum_wheel_forces = np.array(sum_wheel_forces)
+y_forces = np.array(y_forces)
 plt.plot(sum_wheel_forces[:, 0], label="x")
 plt.plot(sum_wheel_forces[:, 1], label="y")
 plt.plot(sum_wheel_forces[:, 2], label="z")
 
 plt.legend()
+
+plt.figure()
+plt.plot(y_forces[:, 0], label = "FL")
+plt.plot(y_forces[:, 1], label = "RL")
+plt.plot(y_forces[:, 2], label = "FR")
+plt.plot(y_forces[:, 3], label = "RR")
+
+plt.legend()
+
 plt.show()
