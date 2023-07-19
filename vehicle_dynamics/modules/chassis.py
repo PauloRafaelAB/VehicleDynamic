684ec6a17a1587cceac4f00f91812bb682999104
Paulo Rafael <paulorafael.ab@gmail.com>
2023-07-19 17:25:01 +0200

    Chassis changes

--- vehicle_dynamics/modules/chassis.py
+++ vehicle_dynamics/modules/chassis.py
@@ -78,37 +78,34 @@ def chassis(parameters: Initialization, logger: logging.Logger, wx: float, wy: f
 
     sum_f_wheel = np.sum(parameters.x_rf.wheel_forces_transformed_force2vehicle_sys, axis = 1)
 
-    logger.debug(f"FORCES IN THE VEHICLE  {np.shape(parameters.x_rf.wheel_forces_transformed_force2vehicle_sys)}")
-    logger.debug(f"Sum wheel {np.shape(sum_f_wheel)}")
+    # logger.debug(f"FORCES IN THE VEHICLE  {np.shape(parameters.x_rf.wheel_forces_transformed_force2vehicle_sys)}")
+    # logger.debug(f"Sum wheel {sum_f_wheel}") # Sum wheel (3,)
     # Equation 11-46 >> 11-12, Pag. 273
     # TODO: check gravity diretion
 
-# =============================================================================
-#     # O que faz esses valores atras da acceleracao?????????????? 11.12-13-14
-#       vx must be the vehicle speed one dimension only
-# =============================================================================
-    drag_x = (parameters.drag * (np.sqrt(parameters.x_a.vx**2 + parameters.x_a.vy**2) * parameters.x_a.vx))
+    drag_x = (-parameters.drag * (np.sqrt(parameters.x_a.vx**2 + parameters.x_a.vy**2) * parameters.x_a.vx))
     drag_y = (parameters.drag * (np.sqrt(parameters.x_a.vx**2 + parameters.x_a.vy**2) * parameters.x_a.vy))
     parameters.x_a.acc_x = ((sum_f_wheel[0] + drag_x) / parameters.car_parameters.m) + ((parameters.x_a.wz * parameters.x_a.vy) - (parameters.x_a.wy * parameters.x_a.vz))
-    parameters.x_a.acc_y = ((sum_f_wheel[1] + drag_y) / parameters.car_parameters.m) + ((parameters.x_a.wx * parameters.x_a.vz) - (parameters.x_a.wz * parameters.x_a.vx))
-    parameters.x_a.acc_z = ((sum_f_wheel[2] - parameters.car_parameters.m * parameters.gravity) / parameters.car_parameters.m) + ((parameters.x_a.wy * parameters.x_a.vx) - (parameters.x_a.wx * parameters.x_a.vy))
+    parameters.x_a.acc_y = ((sum_f_wheel[1] ) / parameters.car_parameters.m) + ((parameters.x_a.wx * parameters.x_a.vz) - (parameters.x_a.wz * parameters.x_a.vx))
+    parameters.x_a.acc_z =  ((sum_f_wheel[2] - parameters.car_parameters.m * parameters.gravity) / parameters.car_parameters.m) + ((parameters.x_a.wy * parameters.x_a.vx) - (parameters.x_a.wx * parameters.x_a.vy))
+    
     parameters.x_a.vx = parameters.x_a.vx + (parameters.x_a.acc_x * parameters.time_step)
     parameters.x_a.vy = parameters.x_a.vy + (parameters.x_a.acc_y * parameters.time_step)
     parameters.x_a.vz = parameters.x_a.vz + (parameters.x_a.acc_z * parameters.time_step)
 
     parameters.x_a.x = parameters.x_a.x + (parameters.x_a.vx * parameters.time_step)
     parameters.x_a.y = parameters.x_a.y + (parameters.x_a.vy * parameters.time_step)
-    parameters.x_a.z = parameters.x_a.z + (parameters.x_a.vz * parameters.time_step)
+    parameters.x_a.z =  parameters.x_a.z + (parameters.x_a.vz * parameters.time_step)
 
     # TODO:Check rolling resistance            
     # rolling_resist = (parameters.car_parameters.fr * parameters.car_parameters.m * parameters.car_parameters.gravity * np.cos(0.) - 0.5 * parameters.car_parameters.row * parameters.car_parameters.Cl * parameters.car_parameters.area * speed ** 2)                              # Rolling Resistance with air lift
-    logger.debug(f"shape position_chassi_force {np.shape(parameters.position_chassi_force)} {np.shape(parameters.strut2chassi_xyz)}")
+    # logger.debug(f"shape position_chassi_force {np.shape(parameters.position_chassi_force)} {np.shape(parameters.strut2chassi_xyz)}")
     
     #parameters.strut2chassi_xyz = parameters.strut2chassi_xyz.T  # na duvida do que isso significa, conferir a matemagica
 
     # TODO Check eq 11 - 47
     crossproduct_r_f = np.zeros(((3,4)))
-    logger.debug(f" position_chassi_force {parameters.position_chassi_force[0,:]} strut2chassi_xyz {parameters.strut2chassi_xyz[:,0]}")
+    logger.info(f" position_chassi_force {np.shape(parameters.position_chassi_force[0,:])} strut2chassi_xyz {parameters.strut2chassi_xyz[:,0]}")
     
     
     crossproduct_r_f[:,0] = np.cross(parameters.position_chassi_force[0,:], parameters.strut2chassi_xyz[:,0])
@@ -116,23 +113,15 @@ def chassis(parameters: Initialization, logger: logging.Logger, wx: float, wy: f
     crossproduct_r_f[:,2] = np.cross(parameters.position_chassi_force[2,:], parameters.strut2chassi_xyz[:,2])
     crossproduct_r_f[:,3] = np.cross(parameters.position_chassi_force[3,:], parameters.strut2chassi_xyz[:,3])
     # logger.debug(f"shape crossproduct_r_f[:,0] {np.shape(crossproduct_r_f[:,0])}")
+    
     sum_crossproduct_r_f = np.sum(crossproduct_r_f,1)
-    logger.debug(f"shape sum_crossproduct_r_f {np.shape(sum_crossproduct_r_f)}")
-    
-    
     #sum_crossproduct_r_f = np.sum(np.cross(parameters.position_chassi_force, parameters.strut2chassi_xyz))
+    logger.debug(f"shape sum_crossproduct_r_f {np.shape(sum_crossproduct_r_f)} sum_crossproduct {sum_crossproduct_r_f}")
+
 
     # TODO make the return of acc angular be type (3,0)
     parameters.x_a.acc_angular_v = (sum_crossproduct_r_f - np.cross(parameters.angular_rates, (parameters.polar_inertia_v @ parameters.angular_rates))) @ np.linalg.inv(parameters.polar_inertia_v)
     
-    
-    
-    
-    
-    
-    
-    
-    logger.debug(f"sum_crossproduct {sum_crossproduct_r_f}")
     # logger.debug('parameters.angular_rates', parameters.angular_rates)
     # logger.debug('polar inertia',parameters.polar_inertia_v)
 
@@ -142,7 +131,7 @@ def chassis(parameters: Initialization, logger: logging.Logger, wx: float, wy: f
     # Angular velocity of the chassis
 
     angular_velocity = parameters.x_a.acc_angular_v * parameters.time_step
-    parameters.x_a.wx = parameters.x_a.wx + angular_velocity[0] 
+    parameters.x_a.wx = parameters.x_a.wx + angular_velocity[0]
     parameters.x_a.wy = parameters.x_a.wy + angular_velocity[1]
     parameters.x_a.wz = parameters.x_a.wz + angular_velocity[2]
 
@@ -180,8 +169,8 @@ def main():
     function_name = test_function.__name__
 
     logger = LocalLogger(function_name).logger
-    logger.setLevel('INFO')
-    parameters = Initialization("../../Audi_r8.yaml", logger=logger)
+    logger.setLevel('DEBUG')
+    parameters = Initialization("C:/Users/albertonbloemer/Documents/VehicleDynamic/Audi_r8.yaml", logger=logger)
     logger.info("loaded Parameters")
 
     path_to_simulation_data = "../../exampledata/acc_brake/SimulationData.pickle"
@@ -195,12 +184,13 @@ def main():
     parameters.x_a.acc_x = sim_data[0].Vhcl_PoI_Acc_x
     parameters.x_a.acc_y = sim_data[0].Vhcl_PoI_Acc_y
     parameters.x_a.acc_z = sim_data[0].Vhcl_PoI_Acc_z
-    parameters.x_a.roll = sim_data[0].Vhcl_Roll
-    parameters.x_a.pitch = sim_data[0].Vhcl_Pitch
-    parameters.x_a.yaw = sim_data[0].Vhcl_Yaw
-
+    parameters.x_a.roll = 0#sim_data[0].Vhcl_Roll
+    parameters.x_a.pitch = 0#sim_data[0].Vhcl_Pitch
+    parameters.x_a.yaw =0  # sim_data[0].Vhcl_Yaw
+    cm_z_force =  parameters.f_za.f_za
+    
     sum_wheels = []
-    range_calc = range(1, 40000)
+    range_calc = range(1, 15000)
     for i in range_calc:
         wz = sim_data[i].Vhcl_YawVel 
         wy = sim_data[i].Vhcl_PitchVel 
@@ -210,9 +200,9 @@ def main():
         az = sim_data[i].Vhcl_PoI_Acc_z
         # parameters.x_a.acc_angular_v = [sim_data[0].Vhcl_RollVel, sim_data[0].Vhcl_PitchVel, sim_data[0].Vhcl_YawVel]
 
-        parameters.x_rf.wheel_forces_transformed_force2vehicle_sys = np.array([[sim_data[i].wheel_load_x_FL, sim_data[i].wheel_load_x_RL, sim_data[i].wheel_load_x_FR, sim_data[i].wheel_load_x_RR],
-                                                                               [sim_data[i].wheel_load_y_FL, sim_data[i].wheel_load_y_RL, sim_data[i].wheel_load_y_FR, sim_data[i].wheel_load_y_RR],
-                                                                               [sim_data[i].wheel_load_z_FL, sim_data[i].wheel_load_z_RL, sim_data[i].wheel_load_z_FR, sim_data[i].wheel_load_z_RR]])
+        parameters.x_rf.wheel_forces_transformed_force2vehicle_sys = np.array([[30,30,30,30], #[sim_data[i].wheel_load_x_FL, sim_data[i].wheel_load_x_RL, sim_data[i].wheel_load_x_FR, sim_data[i].wheel_load_x_RR],
+                                                                               [0,0,0,0], #[sim_data[i].wheel_load_y_FL, sim_data[i].wheel_load_y_RL, sim_data[i].wheel_load_y_FR, sim_data[i].wheel_load_y_RR],
+                                                                               cm_z_force]) #[sim_data[i].wheel_load_z_FL, sim_data[i].wheel_load_z_RL, sim_data[i].wheel_load_z_FR, sim_data[i].wheel_load_z_RR]])
         parameters.strut2chassi_xyz = parameters.x_rf.wheel_forces_transformed_force2vehicle_sys
         parameters.angular_rates = np.array([parameters.x_a.wx,
                                              parameters.x_a.wy,
@@ -228,8 +218,8 @@ def main():
     plt.figure()
     plt.title(function_name)
     plt.plot(range_calc, [i["x_a.pitch"] for i in data], "--", label="pitch")
-    plt.plot(range_calc, [i["x_a.yaw"] for i in data], "--", label="yaw")
-    plt.plot(range_calc, [i["x_a.roll"] for i in data], "--", label="roll")
+    plt.plot(range_calc, [i["x_a.yaw"] for i in data], "x", label="yaw")
+    plt.plot(range_calc, [i["x_a.roll"] for i in data], "+", label="roll")
     var_name = "Vhcl_Pitch"
     plt.plot([i for j, i in enumerate(sim_data.keys()) if j % 10 == 0], [getattr(sim_data[i], var_name) for j, i in enumerate(sim_data) if j % 10 == 0], label = var_name)
     var_name = "Vhcl_Yaw"
