fix delta from chassis rotation

vehicle_dynamics/modules/chassis_rotation.py
@@ -69,29 +69,27 @@ def chassis_rotation(parameters: Initialization, logger: logging.Logger):
 # =============================================================================
 
     # Angular position, velocity, acceleration of the chassis
-    delta = parameters.last_delta       # wheel angle
     Ix = parameters.car_parameters.i_x_s
     Iy = parameters.car_parameters.i_y_s
     Iz = parameters.car_parameters.i_z
     h = parameters.car_parameters.sz
     l = parameters.car_parameters.sr    # halftrack
-    
+
     # Abel Castro, pag. 6 eq-17 
     Fx = sum_f_wheel[0]
     Fy = sum_f_wheel[1]
-    
+
     fy_fl = parameters.x_rf.wheel_forces_transformed_force2vehicle_sys[1, 0]
     fy_rl = parameters.x_rf.wheel_forces_transformed_force2vehicle_sys[1, 1]
     fy_fr = parameters.x_rf.wheel_forces_transformed_force2vehicle_sys[1, 2]
     fy_rr = parameters.x_rf.wheel_forces_transformed_force2vehicle_sys[1, 3]
-    
+
     fx_fl = parameters.x_rf.wheel_forces_transformed_force2vehicle_sys[0, 0]
     fx_rl = parameters.x_rf.wheel_forces_transformed_force2vehicle_sys[0, 1]
     fx_fr = parameters.x_rf.wheel_forces_transformed_force2vehicle_sys[0, 2]
     fx_rr = parameters.x_rf.wheel_forces_transformed_force2vehicle_sys[0, 3]
 
-   
-    Mz = ((fy_fl + fy_fr) * parameters.car_parameters.lv * np.cos(delta) - (fy_rl + fy_rr) * parameters.car_parameters.lh + (fx_fl + fx_fr) * parameters.car_parameters.lv * np.sin(delta) + (fx_rr + fx_fr * np.cos(delta) + fy_fl * np.sin(delta) - fx_rl - fx_fl * np.cos(delta) - fy_fr * np.sin(delta)) * l)
+    Mz = ((fy_fl + fy_fr) * parameters.car_parameters.lv - (fy_rl + fy_rr) * parameters.car_parameters.lh + (fx_rr + fx_fr - fx_rl - fx_fl) * l)
 
     a = Fy * np.cos(parameters.x_a.roll) * np.cos(parameters.x_a.pitch) + parameters.car_parameters.m * parameters.gravity * np.sin(parameters.x_a.roll)
     b = -parameters.car_parameters.c_roll * parameters.x_a.roll - parameters.car_parameters.k_roll * parameters.x_a.wx 
@@ -110,13 +108,10 @@ def chassis_rotation(parameters: Initialization, logger: logging.Logger):
     dd = (Iy * np.cos(parameters.x_a.roll)**2 + Iz * np.sin(parameters.x_a.roll)**2)
 
     parameters.x_a.wy_dot = (aa + bb - cc) / dd
-    
-    
+
     parameters.x_a.wz_dot = (Mz - h * (Fx * np.sin(parameters.x_a.roll) + Fy * np.sin(parameters.x_a.pitch) * np.cos(parameters.x_a.roll))) / (Ix * (np.sin(parameters.x_a.pitch)**2) + (np.cos(parameters.x_a.pitch)**2) * (Iy * (np.sin(parameters.x_a.roll)**2) + Iz * (np.cos(parameters.x_a.roll)**2)))
     #wz_dot = Mz/Iz
-    
-    
-    
+
     parameters.x_a.wx = parameters.x_a.wx + parameters.x_a.wx_dot * parameters.time_step
     parameters.x_a.wy = parameters.x_a.wy + parameters.x_a.wy_dot * parameters.time_step
     parameters.x_a.wz = parameters.x_a.wz + (parameters.x_a.wz_dot * parameters.time_step)
@@ -125,7 +120,7 @@ def chassis_rotation(parameters: Initialization, logger: logging.Logger):
     parameters.x_a.roll = (parameters.x_a.wx * parameters.time_step) + parameters.x_a.roll
     parameters.x_a.pitch = (parameters.x_a.wy * parameters.time_step) + parameters.x_a.pitch
     parameters.x_a.yaw = (parameters.x_a.wz * parameters.time_step) + parameters.x_a.yaw
-    
+
     #parameters.x_a.yaw = ((np.pi+parameters.x_a.yaw)%(2*np.pi))-np.pi
     # TODO check mat mul ordem
     parameters.displacement.za[0] = (- parameters.car_parameters.lv * np.sin(parameters.x_a.pitch)) + (parameters.car_parameters.sl * np.sin(parameters.x_a.roll))
@@ -133,7 +128,7 @@ def chassis_rotation(parameters: Initialization, logger: logging.Logger):
     parameters.displacement.za[2] = (- parameters.car_parameters.lv * np.sin(parameters.x_a.pitch)) - (parameters.car_parameters.sr * np.sin(parameters.x_a.roll))
     parameters.displacement.za[3] = (+ parameters.car_parameters.lh * np.sin(parameters.x_a.pitch)) - (parameters.car_parameters.sr * np.sin(parameters.x_a.roll))
 
-    return parameters, logger#, Mz, wz_dot
+    return parameters, logger, Mz, parameters.x_a.wz_dot
 
 
 def main():
@@ -155,18 +150,18 @@ def main():
 
     parameters.x_a.roll = sim_data[0].Vhcl_Roll
     parameters.x_a.pitch = 0
-    pitch_trans = [sim_data[i].Vhcl_Pitch for i in range(len(sim_data))] # - sim_data[0].Vhcl_Pitch
+    pitch_trans = [sim_data[i].Vhcl_Pitch for i in range(len(sim_data))]  # - sim_data[0].Vhcl_Pitch
     parameters.x_a.yaw = 0
-    yaw_trans = [sim_data[i].Vhcl_Yaw - sim_data[0].Vhcl_Yaw for i in range(len(sim_data))]
+    yaw_trans = [sim_data[i].Vhcl_Yaw for i in range(len(sim_data))]  # sim_data[0].Vhcl_Yaw
     cm_z_force = parameters.f_za.f_za
     parameters.x_a.acc_angular_v = [
         sim_data[0].Vhcl_RollVel, sim_data[0].Vhcl_PitchVel, sim_data[0].Vhcl_YawVel]
 
     sum_wheels = []
     range_calc = range(1, 18000)
-    Mz=[]
-    wz_dot= []
-    
+    Mz = []
+    wz_dot = []
+
     for i in tqdm(range_calc):
         forces_in_x = [sim_data[i].wheel_load_x_FL, sim_data[i].wheel_load_x_RL,
                        sim_data[i].wheel_load_x_FR, sim_data[i].wheel_load_x_RR]
@@ -177,19 +172,16 @@ def main():
         parameters.x_rf.wheel_forces_transformed_force2vehicle_sys = np.array(
             [forces_in_x, forces_in_y, forces_in_z])
 
-        parameters.last_delta = sim_data[i].Steering_angle* parameters.car_parameters.steering_ratio
+        parameters.last_delta = sim_data[i].Steering_angle / parameters.car_parameters.steering_ratio
 
         return_values = test_function(parameters, logger)
 
         data.append(return_values[0].get_data())
-        
+
         Mz.append(return_values[2])
         wz_dot.append(return_values[3])
-        
+
     logger.info("calc end")
-    
-    
-    
 
     plt.figure()
     plt.title(function_name)
@@ -226,17 +218,20 @@ def main():
 
     plt.legend()
 
-
     plt.twinx()
-    
-    plt.plot(range_calc, Mz, "--b", label="Mz")
+
+    #plt.plot(range_calc, Mz, "--b", label="Mz")
 
     plt.legend()
-    
+
+    plt.figure()
+    plt.plot([[sim_data[i].wheel_load_x_FL, sim_data[i].wheel_load_x_RL,
+               sim_data[i].wheel_load_x_FR, sim_data[i].wheel_load_x_RR] for i in range(len(sim_data))])
+
     plt.show()
-    
+
     print(sum(Mz))
-    
+
 
 if __name__ == '__main__':
     main()